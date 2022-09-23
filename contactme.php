b7c2bb287f9e20b0a3af788dbc4669c5bc53cf97
adelfa8219 <108894705+adelfa8219@users.noreply.github.com>
2022-09-23 15:33:22 -0400

    Update contactme.php

--- contactme.php
+++ contactme.php
@@ -13,10 +13,10 @@ $to = "adelfabrito@yahoo.com";
 
 $subject = "Mail from Recruiter";
 $txt ="Name = ". $name .  "\r\n Email = " . $email . "r\n\ Message =" . "$message ." \r\n phone =" . $phone;
-$headers = "From: " . "\r\n" .
+
 
 if($email!=NULL){
-mail($to,$subject,$txt,$headers);
+mail($to,$subject,$txt,$phone);
 }
 
 //redirect
