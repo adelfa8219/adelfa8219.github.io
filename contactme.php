dd6b3acca1408e9458ee8a412aade3e1dbba575c
adelfa8219 <108894705+adelfa8219@users.noreply.github.com>
2022-09-25 17:38:34 -0400

    Update contactme.php

--- contactme.php
+++ contactme.php
@@ -1,23 +1,26 @@
 <?php
-
+if (isset($_POST['SUBMIT'])){
 //get data from form
 	
-$name = $_POST["name"];
-$email = $_POST["email"];
-$phone = $_POST["phone"];
-$answer = $_POST["answer"];
-$message = $_POST["message"];
+$name=$_POST["name"];
+$email=$_POST["email"];
+$phone=$_POST["phone"];
+$answer=$_POST["answer"];
+$message=$_POST["message"];
 
-$to = "adelfabrito@yahoo.com";
+$to="adelfabrito@yahoo.com";
 
 
 $subject = "Mail from Recruiter";
-$txt ="Name = ". $name .  "\r\n Email = " . $email . "r\n\ Message =" . "$message ." \r\n phone =" . $phone;
+$txt ="Name: ".$name.  "\n"."Email: ".$email. "\n". "Message: ".$message."\n". "phone: ".$phone;
+$headers ="From: ".$email;
 
-$headers = "From:noreply@yahoo.com" . "\r\n" .
-"CC: adelfabrito@yahoo.com";
 
-if($email!=NULL){
-mail ($to, $subject, $txt, $phone, $headers);
-header('Location:thankyou.html');}
+if (mail($to, $subject, $txt, $headers)){;
+echo "<h1>Sent Sucessfully! Thank you"." ".$name."; I will contact you shortly!</h1>;
+}
+else{
+echo "something went wrong!";
+}
+}
 ?>
