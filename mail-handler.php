51aded7d7e6b90cd36272b00b5d0a1cede9bda93
adelfa8219 <108894705+adelfa8219@users.noreply.github.com>
2022-09-25 18:43:27 -0400

    Update mail-handler.php

--- mail-handler.php
+++ mail-handler.php
@@ -1,26 +1,23 @@
 <?php
 if (isset($_POST['submit'])){
-//get data from form
 	
-$name=$_POST["name"];
-$email=$_POST["email"];
-$phone=$_POST["phone"];
-$answer=$_POST["answer"];
-$message=$_POST["message"];
+$name=$_POST['name'];
+$email=$_POST['email'];
+$phone=$_POST['phone'];
+$answer=$_POST['answer'];
+$message=$_POST['message'];
 
-$to="adelfabrito@yahoo.com";
-
-
-$subject = "Mail from Recruiter";
-$txt ="Name: ".$name.  "\n"."Email: ".$email. "\n". "Message: ".$message."\n". "phone: ".$phone;
+$to='adelfabrito@yahoo.com';
+$subject = 'Form Submission';
+$txt ="Name: ".$name.  "\n"."Email: ".$email. "\n"."Message: ".$message."\n"."phone: ".$phone;
 $headers ="From: ".$email;
 
 
 if (mail($to, $subject, $txt, $headers)){;
-echo "<h1>Sent Sucessfully! Thank you"." ".$name."; I will contact you shortly!</h1>;
+echo "<h1>Sent Sucessfully! Thank you"." ".$name."; I will contact you shortly!</h1>";
 }
 else{
 echo "something went wrong!";
-}
+  }
 }
 ?>
